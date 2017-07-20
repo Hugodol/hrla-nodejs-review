8c35d4158a0bac09dfb4a54ffbdda7d15ee8198d
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-07-20 11:20:54 -0700

    Use 'url' module for url parsing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 const routes = require('./requestRoutes');
 const request = require('./requests');
 
@@ -15,7 +16,7 @@ const router = {
 const server = http.createServer((req, res) => {
   console.log(`serving request type ${req.method} for url ${req.url}`);
 
-  const route = router[req.url];
+  const route = router[url.parse(req.url).pathname];
 
   route ? route[req.method](req, res) : request.sendResponse(res, '', 404);
 });
